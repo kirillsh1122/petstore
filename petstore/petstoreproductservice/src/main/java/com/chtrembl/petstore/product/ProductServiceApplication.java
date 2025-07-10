7be8a1b6cc170a03f0c432ea4e2aa5521c834fa2
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2025-07-10 20:36:38 +0200

    productservice: added DataPreload bean to ProducServiceApplication

--- petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/ProductServiceApplication.java
+++ petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/ProductServiceApplication.java
@@ -2,9 +2,18 @@ package com.chtrembl.petstore.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.chtrembl.petstore.product.model.DataPreload;
+import com.chtrembl.petstore.product.model.ProductRepository;
 
 @SpringBootApplication
 public class ProductServiceApplication {
+	
+	@Bean
+	public DataPreload dataPreload(ProductRepository productRepository) {
+		return new DataPreload(productRepository);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
