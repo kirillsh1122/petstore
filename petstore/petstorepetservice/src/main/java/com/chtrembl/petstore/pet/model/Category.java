09c1c2f224c7046f727fb3df6c1f75e5bbb580c1
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2025-07-10 20:27:02 +0200

    petservice: modified model classes - JPA entities

--- petstore/petstorepetservice/src/main/java/com/chtrembl/petstore/pet/model/Category.java
+++ petstore/petstorepetservice/src/main/java/com/chtrembl/petstore/pet/model/Category.java
@@ -5,11 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Data
+@Entity
+@Table(name="category", schema="public")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
